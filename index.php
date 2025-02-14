c80cfa03423d9cecc6c7240f2a23bf90e9662f6c
v-alex-dev <vensalex1991@gmail.com>
2025-02-14 22:17:56 +0100

    add index

---
+++ index.php
@@ -0,0 +1,17 @@
+<!--
+=== HTML DISPLAY ===
+-->
+<!doctype html>
+<html lang="fr">
+<head>
+    <?php require_once 'includes/head.php' ?>
+    <title>Blog</title>
+</head>
+<body class="container">
+<?php require_once 'includes/header.php' ?>
+<div class="content">
+
+</div>
+<?php require_once 'includes/footer.php' ?>
+</body>
+</html>
