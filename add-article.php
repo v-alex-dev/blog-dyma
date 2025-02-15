8f6d7a25e5a782e15a2c8d4da38583c4bf6f2e54
v-alex-dev <vensalex1991@gmail.com>
2025-02-15 09:51:06 +0100

    add articles pages

---
+++ add-article.php
@@ -0,0 +1,22 @@
+
+
+
+<!--
+=== HTML DISPLAY ===
+-->
+<!doctype html>
+<html lang="fr">
+<head>
+    <?php include_once 'includes/head.php' ?>
+    <link rel="stylesheet" href="/public/css/add-article.css">
+    <title>Créer un article</title>
+</head>
+<body class="container">
+    <?php include_once 'includes/header.php'?>
+
+    <div class="content">
+
+    </div>
+    <?php include_once 'includes/footer.php' ?>
+</body>
+</html>
