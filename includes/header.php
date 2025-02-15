f975c4c4217233c276c468131a9e9d3b0f684911
v-alex-dev <vensalex1991@gmail.com>
2025-02-15 09:52:19 +0100

    Add navigations menu with interactive class

--- includes/header.php
+++ includes/header.php
@@ -7,5 +7,10 @@
 ?>
 
 <header>
-    <div class="logo">Dyma blog</div>
+    <a href="/"><div class="logo">Dyma blog</div></a>
+    <ul class="header-menu">
+        <li class=<?= $_SERVER['REQUEST_URI'] === '/add-article.php' ? 'active' : '' ?>>
+            <a href="/add-article.php">New Article</a>
+        </li>
+    </ul>
 </header>
