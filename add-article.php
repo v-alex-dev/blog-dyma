762996605b462198eb1ec1fae95ea9363e2cc9dd
v-alex-dev <vensalex1991@gmail.com>
2025-02-28 16:11:44 +0100

    add const errors

--- add-article.php
+++ add-article.php
@@ -1,4 +1,13 @@
+<?php
+    const ERROR_REQUIRED = 'Veuillez renseigner ce champ';
+    const ERROR_CONTENT_TOO_SHORT = 'L\'article est trop court';
+    const ERROR_TITLE_TOO_SHORT = 'Le titre est trop court';
 
+    const ERROR_IMAGE_URL = 'L\'image doit être une url valide';
+
+
+
+?>
 
 
 <!--
